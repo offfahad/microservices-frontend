6af4ece184c3586be20c205f79f6e903529ca855
Muhammad Fahad <mughalfahad544@gmail.com>
2023-01-27 20:35:32 +0500

    microservices-frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AdduserComponent } from './components/adduser/adduser.component';
+import { UpdateuserComponent } from './components/updateuser/updateuser.component';
+import { ViewusersComponent } from './components/viewusers/viewusers.component';
+import { ReactiveFormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdduserComponent,
+    UpdateuserComponent,
+    ViewusersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
